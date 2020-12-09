b857e72a1fa01adee4ddc58ba447099199057162
Nina Van winden <nvan-win@f0r2s13.codam.nl>
2020-12-09 17:34:24 +0100

    Revised for consistency

--- day07/part02.py
+++ day07/part02.py
@@ -1,15 +1,15 @@
-def get_num_bags(color):
+def get_num_bags(color) :
 	i = 0
 	total = 0
     
-	for line in data:
+	for line in data :
 		if color + ' bags contain' in line :
 			rule = line
-	if 'no' in rule:
+	if 'no' in rule :
 		return 1
 	rule = rule.split(' contain')
 	rule = rule[1].split()
-	while i < len(rule):
+	while i < len(rule) :
 		num = int(rule[i]) 
 		color = rule[i + 1] + ' ' + rule[i + 2]
 		total += num * get_num_bags(color)
