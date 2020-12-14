8154f20bade8e5ec29ae46a70063250a6863fa9e
Nina van Winden <ninavanwinden@Ninas-iMac.local>
2020-12-14 19:13:36 +0100

    Solutions to day11

---
+++ day11/part01.py
@@ -0,0 +1,73 @@
+f = open('input.txt', 'r')
+data = f.readlines()
+f.close
+
+data.append('x' * len(data[0]))
+data.insert(0, 'x' * len(data[0]))
+
+grid_border = []
+
+for line in data :
+	grid_border.append('x' + line + 'x')
+
+empty = 'L'
+occupied = '#'
+
+def check_sides(layout, y, x) :
+	count = 0
+	if layout[y-1][x-1] == occupied :
+		count += 1
+	if layout[y-1][x] == occupied :
+		count += 1
+	if layout[y-1][x+1] == occupied :
+		count += 1
+	if layout[y][x-1] == occupied :
+		count += 1
+	if layout[y][x+1] == occupied :
+		count += 1
+	if layout[y + 1][x-1] == occupied :
+		count += 1
+	if layout[y + 1][x] == occupied :
+		count += 1
+	if layout[y + 1][x+1] == occupied :
+		count += 1
+	return count
+
+def check_grid(seat_layout) :
+	new_layout = seat_layout.copy()
+	y = 0
+	for line in seat_layout :
+		x = 0
+		for char in line :
+			if char == empty :
+				count = check_sides(seat_layout, y, x)
+				if count == 0 :
+					new_layout[y] = new_layout[y][:x] + occupied + new_layout[y][x+1:]
+			elif char == occupied :
+				count = check_sides(seat_layout, y, x)
+				if count >= 4 :
+					new_layout[y] = new_layout[y][:x] + empty + new_layout[y][x+1:]
+			x += 1
+		y += 1
+	return(new_layout)
+
+def get_num_occupied(seat_layout) :
+	count = 0
+	for line in seat_layout:
+		for char in line :
+			if char == occupied :
+				count += 1
+	return (count)
+
+def comp_layout() :
+	seat_layout_1 = []
+	seat_layout_2 = grid_border.copy()
+
+	while seat_layout_1 != seat_layout_2 :
+		seat_layout_1 = seat_layout_2.copy()
+		seat_layout_2 = check_grid(seat_layout_1)
+
+	return get_num_occupied(seat_layout_1)
+
+answer = comp_layout()
+print(answer)
